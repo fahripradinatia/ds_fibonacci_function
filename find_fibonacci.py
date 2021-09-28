ad04034c192b22b90de9874ca8c93890ce98819b
fahripradinatia <fahripradinatia@gmail.com>
2021-09-28 21:34:50 +0700

    Add New Function find_fibonacci, if x in fibonacci numbers will return message True, if not will return message False

--- find_fibonacci.py
+++ find_fibonacci.py
@@ -5,7 +5,15 @@ def find_fibonacci(x: int) -> bool:
     Jika tidak ada, maka kembalikan False
     """
     # write your code here
+    fibonacci_numbers = [0,1]
 
+    for i in range(2,20):
+      fibonacci_numbers.append(fibonacci_numbers[i-1] + fibonacci_numbers[i-2])
+
+    if x in fibonacci_numbers:
+      return True
+    else:
+      return False
 
 if __name__ == "__main__":
     """Jalankan beberapa test-case di bawah sini
